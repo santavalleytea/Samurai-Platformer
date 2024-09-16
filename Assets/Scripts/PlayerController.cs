bb353dc823ad18f9a59f6de5a24d0683819046a2
Takaya Ueno <takayaueno0712@gmail.com>
2024-09-16 19:08:54 +1000

    Added attack and jump animation, and background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Animator animator;
+    private Rigidbody2D rb;
 
-    float speed = 5.0f;
+    public float speed = 5.0f;
+    public float jumpForce = 0.5f;
+    public LayerMask groundLayer;
+    private bool isGrounded = false;
 
     int isRunningHash;
-    int isAttackingHash;
+    int attackTriggerHash;
+    int jumpTriggerHash;
 
     void Start() {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 
         isRunningHash = Animator.StringToHash("isRunning");
-        isAttackingHash = Animator.StringToHash("IsAttacking");
+        attackTriggerHash = Animator.StringToHash("attackTrigger");
+        jumpTriggerHash = Animator.StringToHash("jumpTrigger");
 
     }
 
@@ -22,25 +29,49 @@ public class PlayerController : MonoBehaviour {
         bool rightKey = Input.GetKey(KeyCode.D);
         bool leftKey = Input.GetKey(KeyCode.A);
         bool attackKey = Input.GetKey(KeyCode.Mouse0);
+        bool jumpKey = Input.GetKeyDown(KeyCode.Space);
 
         bool isRunning = rightKey || leftKey;
-        
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
         animator.SetBool(isRunningHash, isRunning);
 
         if (rightKey) {
+            // Turn right then move
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
         } else if (leftKey) {
+            // Turn left then move
             transform.rotation = Quaternion.Euler(0, 180, 0);
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
         }
 
+        // Attack key trigger
         if (attackKey) {
-            animator.SetBool(isAttackingHash, true);
+            animator.SetTrigger(attackTriggerHash);
         } 
 
-        if (!attackKey) {
-            animator.SetBool(isAttackingHash, false);
+        if (stateInfo.IsName("Attack1")) {
+            animator.ResetTrigger(attackTriggerHash);
+        }
+
+        //isGrounded = IsGrounded();
+
+        if (jumpKey && isGrounded) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetTrigger(jumpTriggerHash);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
         }
     }
 }
